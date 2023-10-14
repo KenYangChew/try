92ec5d3fc3407b5009fbacf0566a86a159b69fbd
KenYangChew <126490088+KenYangChew@users.noreply.github.com>
2023-10-15 01:56:48 +0800

    Create vega_lite_vis.js

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,15 @@
+vegaEmbed('#vis1', spec1, { "actions": false }).then(function (result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed('#vis2', spec2, { "actions": false }).then(function (result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+        
+vegaEmbed('#vis3', spec3, { "actions": false }).then(function (result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+        
+vegaEmbed('#vis4', spec4, { "actions": false }).then(function (result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
