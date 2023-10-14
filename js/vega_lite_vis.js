a926243cec0db35906d26831d1e9cb415833a4f6
KenYangChew <126490088+KenYangChew@users.noreply.github.com>
2023-10-15 02:02:41 +0800

    Update vega_lite_vis.js

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -3,18 +3,18 @@ var spec2 = "js/splot.vg.json";
 var spec3 = "js/import.vg.json";
 var spec4 = "js/export.vg.json";    
 
-vegaEmbed('#vis1', spec1, { "actions": false }).then(function (result) {
+vegaEmbed('#vis1', visualisation).then(function (result) {
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed('#vis2', spec2, { "actions": false }).then(function (result) {
+vegaEmbed('#vis2', splot).then(function (result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
         
-vegaEmbed('#vis3', spec3, { "actions": false }).then(function (result) {
+vegaEmbed('#vis3', imports).then(function (result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
         
-vegaEmbed('#vis4', spec4, { "actions": false }).then(function (result) {
+vegaEmbed('#vis4', exports).then(function (result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
